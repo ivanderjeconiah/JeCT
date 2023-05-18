820d9f5ddb3d1642fe13cb49d3801b6ba949e21b
ivanderjeconiah <78361298+ivanderjeconiah@users.noreply.github.com>
2023-05-18 22:46:08 +0700

    Update P1.ino

--- P1.ino
+++ P1.ino
@@ -8,10 +8,11 @@
 float voltageSensor;
 bool in1,in2;
 uint8_t in1Val, in2Val;
-int timer, rele1Timer,rele2Timer; 
+long long timer, rele1Timer,rele2Timer; 
 float batas;
 bool second=false;
 bool rele1Stat=false,rele2Stat=false;
+bool switch1Val,switch2Val;
 
 
 void Volt(){
@@ -25,10 +26,11 @@ void setup(){
   Serial.println("Starting");
   pinMode(RELE1, OUTPUT);
   pinMode(RELE2, OUTPUT);
-  digitalWrite(RELE1,HIGH);
-  digitalWrite(RELE2,HIGH);
+  digitalWrite(RELE1,LOW);
+  digitalWrite(RELE2,LOW);
   pinMode(SW1, INPUT_PULLUP);
   pinMode(SW2, INPUT_PULLUP);
+  delay(1000);
   in1=digitalRead(SW1);
   in2=digitalRead(SW2);
   Serial.println(in1);
@@ -36,11 +38,13 @@ void setup(){
 }
 
 void loop(){
-  if ((in1!=digitalRead(SW1)) && (in2!=digitalRead(SW2))){
+  switch1Val=digitalRead(SW1);
+  switch2Val=digitalRead(SW2);
+  if ((in1!=switch1Val) && (in2!=switch2Val) && (switch1Val!=switch2Val)){
+    in1=switch1Val;
+    in2=switch2Val;
     Serial.println(in1);
     Serial.println(in2);
-    in1=digitalRead(SW1);
-    in2=digitalRead(SW2);
     if(in1){
       in1Val=0;
       in2Val=5;
@@ -55,28 +59,31 @@ void loop(){
   }
 
   if (second){
+    //Serial.println(millis()-timer);
     if(millis()-timer<60000){
       Volt();
       Serial.println(voltageSensor);
       if(in1Val-voltageSensor>=batas){
         second=false;
-        digitalWrite(RELE1,LOW);
+        digitalWrite(RELE1,HIGH);
         rele1Stat=true;
         rele1Timer=millis();
       }
       else if (in2Val-voltageSensor>=batas){
         second=false;
-        digitalWrite(RELE2,LOW);
+        digitalWrite(RELE2,HIGH);
         rele2Stat=true;
         rele2Timer=millis();
       }
     }
     else {
       batas=batas-0.5;
+      Serial.println(batas);
       if(batas<3){
         second=false;
       }
       else{
+        Serial.println("reset timer");
         timer=millis();
       }   
     }
@@ -85,14 +92,14 @@ void loop(){
   if(rele1Stat){
     if(millis()-rele1Timer>=3000){
       rele1Stat=false;
-      digitalWrite(RELE1, HIGH);
+      digitalWrite(RELE1, LOW);
     }
   }
 
   if(rele2Stat){
     if(millis()-rele2Timer>=3000){
       rele2Stat=false;
-      digitalWrite(RELE2,HIGH);
+      digitalWrite(RELE2,LOW);
     }
   }
 
